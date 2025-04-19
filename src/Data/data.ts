8cc4288a0ed938738c152574f1dee8ad7ce15c7f
Adam-Elmi <Adamcade123@gmail.com>
2025-04-19 23:08:14 +0300

    Added Data dir

---
+++ src/Data/data.ts
@@ -0,0 +1,18 @@
+export const advantages_of_somdep = [
+  {
+    title: "Ka hortaga khaladaadka",
+    description: " Wuxu ka ilaalinaya inaad isticmaasho code qaladaad/khaladaad keeni kara."
+  },
+  {
+    title: "Isticmaalka waxyaabaha cusub",
+    description: "Wuxu ku tusaya waxyaabaha hada la isticmaalo iyo waxyaabihi laga tagey isticmaalkooda."
+  },
+  {
+    title: "Kobcinta Tayada Code-ka",
+    description: "Wuxu ka ilaalinaya isticmaalka syntax ama farsamooyin aan la taageerin."
+  },
+  {
+    title: "Offline ayuu ayaad isticmaali karta",
+    description: "Xogta SomDep waxaad uga baaraandegi kartaa terminal (CLI tool) adigoon internet u baahnayn."
+  },
+];
