a288e5ad10512c84b3af9ec95ac10d6c30a7b54b
Adam-Elmi <Adamcade123@gmail.com>
2025-05-04 02:45:21 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import 'highlight.js/styles/github-dark.css';
 import Home from "./pages/Home";
 import AllFiles from "./pages/Source";
 import { Routes, Route } from "react-router";
+import Contents from "./pages/Content";
+import NotFound from "./Components/404";
 
 function App() {
   return (
     <Routes>
       <Route element={<Home/>} path="/"/>
       <Route element={<AllFiles/>} path="/source"/>
+      <Route element={<Contents/>} path="/contents/:id"/>
+      <Route element={<NotFound/>} path="*"/>
     </Routes>
   )
 }
