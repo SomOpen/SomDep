8cabda5165d16ca300d2a37bef2f353c8efa3366
Adam-Elmi <Adamcade123@gmail.com>
2025-05-06 18:14:52 +0300

    Modified Layout component

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,7 +1,12 @@
 import Nav from "./Nav";
 import SideBar from "./SideBar";
 import Footer from "./Footer";
-export default function Layout({children}: React.PropsWithChildren<{}>) {
+
+type Props = {
+  children: React.ReactNode,
+  allow_bg?: boolean
+}
+export default function Layout({children, allow_bg}: Props) {
     return (
       <div className="relative h-screen">
         {/* Left Side */}
@@ -9,7 +14,7 @@ export default function Layout({children}: React.PropsWithChildren<{}>) {
         {/* Right Side */}
         <div className="ml-[250px] max-[850px]:ml-0 max-w-full flex flex-col h-full">
           <Nav show_top_layer={false} show_source={false} show_menu_btn={true} />
-            <main className="flex-1">
+            <main className={`flex-1 ${allow_bg ? "bg-gradient-to-r from-blue-50 via-gray-100 to-white" : ""}`}>
                 {children}
             </main>
           <Footer />
