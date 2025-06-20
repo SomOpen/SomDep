37a10ca94e5bf56b21b2a5240566167a268eafb5
Adam-Elmi <Adamcade123@gmail.com>
2025-06-20 14:03:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import Home from "./pages/Home";
 import { Routes, Route } from "react-router";
 import Contents from "./pages/Content";
 import NotFound from "./Components/404";
-import View from "./pages/View";
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/view" element={<View />} />
       <Route path="/contents/:id" element={<Contents />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
